7e8299d124708aecc16bf5e7f8241db62de3ed56
Aman Mishra <thegreatestaman999@gmail.com>
2026-02-12 20:19:12 +0530

    u

--- routes/scanRoutes.js
+++ routes/scanRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const scanController = require('../controllers/scanController');
+
+// Import auth middleware correctly
 const { verifyToken } = require('../middleware/authMiddleware');
 
 // All routes need host authentication
@@ -9,7 +11,6 @@ router.use(verifyToken);
 router.post('/verify', scanController.verifyTicket);
 router.get('/fest-stats/:festId', scanController.getFestStats);
 router.get('/recent-scans/:festId', scanController.getRecentScans);
-// Add to scanRoutes.js
 router.post('/log-denied', scanController.logDenied);
 
 module.exports = router;
