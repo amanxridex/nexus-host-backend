6c90128fd9a0ae072c70603bc01c3413a04c0101
Aman Mishra <thegreatestaman999@gmail.com>
2026-04-12 15:49:45 +0530

    fix(routes): replace verifyToken with verifyHostSession in restaurantRoutes to fix Router.use crash

--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { createRestaurant, getMyRestaurants } = require('../controllers/restaurantController');
-const { verifyToken } = require('../middleware/authMiddleware');
+const { verifyHostSession } = require('../middleware/authMiddleware');
 
-router.use(verifyToken);
+router.use(verifyHostSession);
 
 router.post('/', createRestaurant);
 router.get('/mine', getMyRestaurants);
